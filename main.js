f417a8ffe1e99d9399359405b7177e19d511afb1
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-27 17:18:48 +0100

    aggiunto click

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 
 $(document).ready(function() {
-  intercetto li click sull'icona freccia
-  alert('ciao')
-  $('a i').click(function(){
-    alert('ciao')
-  }
+  // intercetto li click sull'icona freccia
 
-  devo assegnare la classe active alle altre immagini
-  determino l'immagine corrente
-  var currentImg = $('.active')
+  // devo assegnare la classe active alle altre immagini
+  // determino l'immagine corrente
+  var currentImg = $('img.visible');
 
+  $('.forward a').click(function() {
+
+    currentImg.removeClass('visible');
+
+    currentImg.next().addClass('visible')
+
+
+
+
+  });
+
+  $('.backward a').click(function(){
+
+    currentImg.removeClass('visible');
+
+    currentImg.prev().addClass('visible');
+  });
 
 });
-
-$('.backward').click(function() {
-
-  alert('ciao')
-})
-
-$('.bottone').click(function(){
-
-  alert('ciao')
-$('.bottone').click( function() { alert('clicked'); });
-})
