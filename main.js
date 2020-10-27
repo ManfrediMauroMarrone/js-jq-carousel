38836104f86e94c468a5dc6f654f383f18e79fb3
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-27 16:58:50 +0100

    decommentato document ready

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
 
-// $(document).ready(function() {
-//   // intercetto li click sull'icona freccia
-//   alert('ciao')
-//   // $('a i').click(function(){
-//   //   alert('ciao')
-//   // }
-//
-//   // devo assegnare la classe active alle altre immagini
-//   // determino l'immagine corrente
-//   // var currentImg = $('.active')
-//
-//
-// });
+$(document).ready(function() {
+  intercetto li click sull'icona freccia
+  alert('ciao')
+  $('a i').click(function(){
+    alert('ciao')
+  }
 
-// $('.backward').click(function() {
-//
-//   alert('ciao')
-// })
-//
-// $('.bottone').click(function(){
-//
-//   alert('ciao')
-// })
+  devo assegnare la classe active alle altre immagini
+  determino l'immagine corrente
+  var currentImg = $('.active')
 
+
+});
+
+$('.backward').click(function() {
+
+  alert('ciao')
+})
+
+$('.bottone').click(function(){
+
+  alert('ciao')
 $('.bottone').click( function() { alert('clicked'); });
-
-alert('ciao')
+})
