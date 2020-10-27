a6db4dfc8251bc199cdd53306c2a3bb89d529e22
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-27 19:08:28 +0100

    modifiche js

--- main.js
+++ main.js
@@ -4,24 +4,70 @@ $(document).ready(function() {
 
   // devo assegnare la classe active alle altre immagini
   // determino l'immagine corrente
-  var currentImg = $('img.visible');
 
   $('.forward a').click(function() {
 
+    var currentImg = $('img.visible');
+
     currentImg.removeClass('visible');
 
-    currentImg.next().addClass('visible')
+    if(currentImg.next('img').length){
 
+      currentImg.next('img').addClass('visible')
 
+    } else {
+
+      $('.main :first-child').addClass('visible')
+
+    };
+
+    var currentDot = $('span.active')
+
+    // rimuovo active dal pallino
+
+    currentDot.removeClass('active')
+    // aggiungo active al pallino successivo
+
+    if (currentDot.next('span').length) {
+      currentDot.next('span').addClass('active')
+
+    } else {
+      $('.bullet :first-child').addclass('active')
+    }
 
 
   });
 
   $('.backward a').click(function(){
 
+    var currentImg = $('img.visible');
+
     currentImg.removeClass('visible');
 
-    currentImg.prev().addClass('visible');
+    if (currentImg.prev('img').length) {
+
+      currentImg.prev('img').addClass('visible');
+
+    } else {
+
+      $('.main img:last-of-type').addClass('visible');
+
+    }
+
+    var currentDot = $('span.active')
+
+    // rimuovo active dal pallino
+
+    currentDot.removeClass('active')
+    // aggiungo active al pallino successivo
+    if (currentDot.prev('span').length) {
+      currentDot.prev('span').addClass('active')
+
+    } else {
+      $('.bullet span:last-of-type').addclass('active')
+    }
+
+
   });
 
 });
