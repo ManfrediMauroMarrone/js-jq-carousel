dccd1eb863913d249a699c98c0efc6267555e72d
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-27 16:51:19 +0100

    aggiunto jquery

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 
-$(document).ready(function() {
-  // devo assegnare la classe active alle altre immagini
-  // determino l'immagine corrente
-  var currentImg
+// $(document).ready(function() {
+//   // intercetto li click sull'icona freccia
+//   alert('ciao')
+//   // $('a i').click(function(){
+//   //   alert('ciao')
+//   // }
+//
+//   // devo assegnare la classe active alle altre immagini
+//   // determino l'immagine corrente
+//   // var currentImg = $('.active')
+//
+//
+// });
 
+// $('.backward').click(function() {
+//
+//   alert('ciao')
+// })
+//
+// $('.bottone').click(function(){
+//
+//   alert('ciao')
+// })
 
-});
+$('.bottone').click( function() { alert('clicked'); });
+
+alert('ciao')
